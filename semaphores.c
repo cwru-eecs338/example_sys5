4c59ca139a7cd9524fa394ad3f01cd368814594c
Gary Doran, Jr <garydoranjr@gmail.com>
2011-02-07 18:56:04 -0500

    Create and remove shared memory

--- semaphores.c
+++ semaphores.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
 
 const int BUF_SIZE = 3;
 enum SEMAPHORES {MUTEX = 0, EMPTY, FULL, NUM_SEM};
@@ -18,11 +19,27 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    // Create shared memory (similar arguments as semget)
+    size_t shmsize = BUF_SIZE;
+    int shmid = shmget(IPC_PRIVATE, shmsize, IPC_CREAT | 0666);
+    if (shmid < 0) {
+        perror("Error getting semaphores");
+        exit(EXIT_FAILURE);
+    }
+
     // For semaphore group with semkey
     // (Second argument ignored)
     // remove it (IPC_RMID)
     if (semctl(semkey, 0, IPC_RMID) < 0) {
         perror("Error removing semaphores");
+        exit(EXIT_FAILURE);
+    }
+
+    // For shared memory segment with id shmid
+    // remove it (IPC_RMID)
+    // (last argument ignored, use NULL pointer)
+    if (shmctl(shmid, IPC_RMID, (struct shmid_ds *) NULL)) {
+        perror("Error removing shared memory");
         exit(EXIT_FAILURE);
     }
 
